d09f7d58892742337a42638d8f621f1331c586c2
Adrien B <forbu14@gmail.com>
2023-05-24 22:30:43 +0000

    checker

--- src/transformers/utils/generic.py
+++ src/transformers/utils/generic.py
@@ -557,6 +557,7 @@ def add_model_info_to_auto_map(auto_map, repo_id):
     Adds the information of the repo_id to a given auto map.
     """
     for key, value in auto_map.items():
+        print("key", key, "value", value)
         if isinstance(value, (tuple, list)):
             auto_map[key] = [f"{repo_id}--{v}" if (v is not None and "--" not in v) else v for v in value]
         elif value is not None and "--" not in value:
